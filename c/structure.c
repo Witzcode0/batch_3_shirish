d7f45e22c681e03c20c59fc8f32368a28525b1fa
Witzcode0 <brijesh.gondaliya07@gmail.com>
2025-08-02 10:03:26 +0530

    tokens

---
+++ c/structure.c
@@ -0,0 +1,33 @@
+// documentation section
+// sum of 2 numbers
+
+// linking section
+#include<stdio.h>
+#include<conio.h>
+
+// global declaration section
+int num1 = 10;
+
+// main function
+void main(){
+    // local declaration section
+    int num2 = 20;
+
+    printf("%d", num1 + num2);
+
+}
+
+// sub function declaration section
+
+// Example:
+
+
+
+// single line comment
+/*
+multiple line comments
+this is line 1
+this is line 2
+*/
+
+
