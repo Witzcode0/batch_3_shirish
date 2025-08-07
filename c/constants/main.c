5c014364a43637b0adf976a54d2398e7ae7b9b81
Witzcode0 <brijesh.gondaliya07@gmail.com>
2025-08-07 09:06:05 +0530

    constant

---
+++ c/constants/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Define PI as a macro
+#define PI 3.14
+
+void main() {
+
+    float r = 10.0;
+    float area = PI * r * r;
+
+    printf("Area of circle : %f\n", area);
+}
