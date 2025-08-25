0f22e7d1c6bba782e6954c0d15f390cc96f1e290
Witzcode0 <brijesh.gondaliya07@gmail.com>
2025-08-25 10:10:35 +0530

    function

---
+++ c/functions/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int sum(int num1,int num2){
+    return num1 + num2;
+};
+
+void  main(){
+    // while(1){
+       // int n1, n2;
+       // printf("\nEnter a num1 and num2 : ");
+       // scanf("%d %d", &n1, &n2);
+// printf("res: %d", sum(n1, n2)+ 5);
+    //}
+
+
+    int resofsum = sum(10, 20);
+    printf("sum : %d", resofsum);
+}
